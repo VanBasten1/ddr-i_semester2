e6c0e9a3e80cae57a0d0f83b929815fd38fa2792
daffa raihan ramadhan <140674037+VanBasten1@users.noreply.github.com>
2024-01-25 08:52:53 +0700

    Add files via upload

---
+++ pertemuan_3/operator_keanggotaan.py
@@ -0,0 +1,7 @@
+buah = ["apel", "pisang", "anggur", "durian"]
+
+#operator membership (IN)
+print("durian" in buah)
+
+#operator membership (not in)
+print("apel" not in buah)
---
+++ pertemuan_3/operator_penugasan.py
@@ -0,0 +1,29 @@
+#pertambahan
+a = 5
+a += 3
+print(a)
+
+#penguragan
+b = 8
+b -= 3
+print=(b)
+
+#perkalian 
+c = 5
+c *= 2
+print=(c)
+
+#pembagian
+d = 50
+d /= 5
+print=(d)
+
+#perpangkatan
+e = 3 
+e **= 5
+print=(e)
+
+#modulus
+f = 6
+f %= 2
+print=(f)
