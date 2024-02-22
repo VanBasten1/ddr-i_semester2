2dd6e2bf89dbe7108d36712db57e7b8c0ea88986
daffa raihan ramadhan <140674037+VanBasten1@users.noreply.github.com>
2024-02-22 09:20:51 +0700

    Add files via upload

---
+++ Pertemuan_6/perkenalan.py
@@ -0,0 +1,8 @@
+def pembukaan():
+    print("saya siap belajar")
+
+def perkenalan(nama,kelas):
+    print("Nama saya", nama, "dan saya kelas", kelas)
+
+pembukaan()
+perkenalan("Daffa Raihan R", "RPL X-B")
